d7e0d5eb1fd59af177ca23718b48377fbbf6271a
NeulovimiyDjo <NeulovimiyDjo@localhost>
2026-04-06 04:18:37 +0300

    upd1

--- DotNet/Utilities/LogEventReader.cs
+++ DotNet/Utilities/LogEventReader.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Globalization;
 using System.IO;
+using System.Linq;
 using System.Text;
 using System.Text.RegularExpressions;
 
@@ -11,6 +12,7 @@ namespace LogEventReaderProject
     {
         private static string LoggerName => $"{nameof(LogEventReader<TEvent>)}<{typeof(TEvent).Name}>";
 
+        private int _lastLatestFileNumber = -1;
         private long _lastReadLength = 0;
         private string _lastUnhandledTextPiece = "";
         private readonly Queue<TEvent> _logEventsQueue = new();
@@ -30,11 +32,10 @@ namespace LogEventReaderProject
 
         public void ReadNew()
         {
-            // These logs are expected to be empty on service start. Otherwise entries may be lost on log rotate right after start.
-            string logFilesPathBase = Path.Join(
-                Path.GetDirectoryName(_logFile),
-                Path.GetFileNameWithoutExtension(_logFile));
-            GetEventsFromLogs(logFilesPathBase, ref _lastReadLength, ref _lastUnhandledTextPiece);
+            // These logs are expected to be empty on chronos/web start. Otherwise entries may be lost on log rotate right after start.
+            string logFilesDir = Path.GetDirectoryName(_logFile)!;
+            string logFilesNameBase = Path.GetFileName(_logFile);
+            GetEventsFromLogs(logFilesDir, logFilesNameBase, ref _lastReadLength, ref _lastUnhandledTextPiece);
         }
 
         public bool TryDequeue(out TEvent logEvent)
@@ -45,13 +46,87 @@ namespace LogEventReaderProject
         }
 
         private void GetEventsFromLogs(
-            string logFilesPathBase,
+            string logFilesDir, string logFilesNameBase,
             ref long lastReadLength, ref string lastUnhandledTextPiece)
         {
+            (string? latestLogFilePath, string? previousLogFilePath) = GetLatestAndPreviousLogFilesPaths();
+            if (latestLogFilePath is null)
+                return;
+
+            try
+            {
+                long latestFileSize = new FileInfo(latestLogFilePath).Length;
+                if (latestFileSize < lastReadLength)
+                {
+                    if (previousLogFilePath is null)
+                        throw new Exception($"File '{latestLogFilePath}' has just been rotated, but '{previousLogFilePath}' doesn't exist.");
+                    Loggers.Monitoring.Debug($"{LoggerName}: File '{latestLogFilePath}' has just been rotated.");
+
+                    long previousFileSize = new FileInfo(previousLogFilePath).Length;
+                    if (previousFileSize > lastReadLength)
+                    {
+                        string text = ReadLastText(previousLogFilePath, ref lastReadLength);
+                        Loggers.Monitoring.Trace($"{LoggerName}: Previous file text read:\n{text}");
+                        OnTextRead(text, ref lastUnhandledTextPiece);
+                    }
+                    lastReadLength = 0;
+                }
+
+                if (latestFileSize > lastReadLength)
+                {
+                    string text = ReadLastText(latestLogFilePath, ref lastReadLength);
+                    Loggers.Monitoring.Trace($"{LoggerName}: Latest file text read:\n{text}");
+                    OnTextRead(text, ref lastUnhandledTextPiece);
+                }
+            }
+            catch (Exception ex)
+            {
+                Loggers.Monitoring.LogException($"{LoggerName}: Error monitoring file:", ex);
+            }
+
+            (string? latestLogFilePath, string? previousLogFilePath) GetLatestAndPreviousLogFilesPaths()
+            {
+                if (!Directory.Exists(logFilesDir))
+                    return (null, null);
+
+                HashSet<string> logFilesNames = [.. Directory.GetFiles(
+                    logFilesDir, $"{logFilesNameBase}*.txt", SearchOption.TopDirectoryOnly)
+                        .Select(filePath => Path.GetFileName(filePath)),];
+
+                int i = _lastLatestFileNumber == -1 ? 9999 : _lastLatestFileNumber + 1;
+                string? latestLogFileName = null;
+                string? previousLogFileName = null;
+                while (latestLogFileName is null && i >= 0)
+                {
+                    string fileName = GetLogFileName(i);
+                    if (logFilesNames.Contains(fileName))
+                    {
+                        latestLogFileName = fileName;
+                        _lastLatestFileNumber = i;
+                    }
+                    i--;
+                }
+                if (latestLogFileName is not null && logFilesNames.Contains(GetLogFileName(i)))
+                    previousLogFileName = GetLogFileName(i);
+
+                string? latestLogFilePath = latestLogFileName is null ? null : Path.Join(logFilesDir, latestLogFileName);
+                string? previousLogFilePath = previousLogFileName is null ? null : Path.Join(logFilesDir, previousLogFileName);
+                return (latestLogFilePath, previousLogFilePath);
+
+                string GetLogFileName(int i) => i == 0 ? $"{logFilesNameBase}.txt" : $"{logFilesNameBase}_{i:0000}.txt";
+            }
+        }
+
+        private void GetEventsFromLogs_OldRotationLogic(
+            string logFilesDir, string logFilesNameBase,
+            ref long lastReadLength, ref string lastUnhandledTextPiece)
+        {
+            string logFilesPathBase = Path.Join(logFilesDir, logFilesNameBase);
             string mainLogFilePath = $"{logFilesPathBase}.txt";
             string previousLogFilePath = $"{logFilesPathBase}.0.txt";
             if (!File.Exists(mainLogFilePath))
                 return;
+
             try
             {
                 long mainFileSize = new FileInfo(mainLogFilePath).Length;
